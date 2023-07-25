f7e30f5a9499d70960cdb13f8d0dbe775a6c171e
SalonikaPanda <134544828+SalonikaPanda@users.noreply.github.com>
2023-07-25 12:57:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let snakeX = 5, snakeY = 5;
 let velocityX = 0, velocityY = 0;
 let snakeBody = [];
 let setIntervalId;
-let hitSound=false;
+let hitSoundPlayed=false;
 let score = 0;
 let highScore = localStorage.getItem("high-score") || 0;
 highScoreElement.innerText = `High Score: ${highScore}`;
